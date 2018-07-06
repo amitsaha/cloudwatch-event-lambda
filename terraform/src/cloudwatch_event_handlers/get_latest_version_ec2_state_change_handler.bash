0fe74940a0e49ce2327f9040d152b5f6625ca74f
Amit Saha <amitsaha.in@gmail.com>
2018-07-06 20:25:08 +1000

    update

--- terraform/src/cloudwatch_event_handlers/get_latest_version_ec2_state_change_handler.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-aws s3api head-object --bucket aws-health-notif-demo-lambda-artifacts --key ec2-state-change/src.zip 
--- terraform/src/cloudwatch_event_handlers/get_latest_version_health_event_handler.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-aws s3api head-object --bucket aws-health-notif-demo-lambda-artifacts --key health-event/src.zip 
