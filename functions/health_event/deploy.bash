b586e97a72bdb96446c7876452ff702c48ae688e
Amit Saha <amitsaha.in@gmail.com>
2018-07-23 06:02:38 +0000

    Fixes

--- functions/health_event/deploy.bash
+++ functions/health_event/deploy.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -ex
+set -e
 
 pushd src
 zip -r ../src.zip *
@@ -9,10 +9,16 @@ aws s3 cp src.zip s3://aws-health-notif-demo-lambda-artifacts/health-event/src.z
 version=$(aws s3api head-object --bucket aws-health-notif-demo-lambda-artifacts --key health-event/src.zip)
 version=$(echo $version | python -c 'import json,sys; obj=json.load(sys.stdin); print(obj["VersionId"])')
 
+if [ -z "$HEALTH_EVENT_WEBHOOK_URL" ]; then
+    echo "HEALTH_EVENT_WEBHOOK_URL not specified"
+    exit 1
+fi
+
+
 # Deploy to demo environment
 pushd ../../terraform/environments/demo
 bash tf.bash cloudwatch_event_handlers apply \
-    -var health_event_handler_version=$version \
+    -var health_event_notify_handler_version=$version \
     -var health_event_webhook_url=$HEALTH_EVENT_WEBHOOK_URL \
     -target=aws_lambda_function.health_event \
     -target=aws_lambda_permission.health_event_cloudwatch \
--- functions/health_event/src/main.py
+++ functions/health_event/src/main.py
@@ -3,7 +3,7 @@ import sys
 import json
 
 CWD = os.path.dirname(os.path.realpath(__file__))
-sys.path.insert(0, os.path.join(CWD, "lib"))
+sys.path.insert(0, os.path.join(CWD, "libs"))
 
 import requests
 
